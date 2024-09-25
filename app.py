8308d13f38f3ad8f6639db70581ad46f77251c35
thiagomarchesi <181411010+thiagomarchesi@users.noreply.github.com>
2024-09-25 12:28:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if hist_button:
     
     st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
       
-    fig = px.histogram(car_data, x="price", nbins=20, alpha=0.5)
+    fig = px.histogram(car_data, x="price", nbins=20)
     
     st.plotly_chart(fig, use_container_width=True)
 
