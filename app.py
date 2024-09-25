b1b5fff9a39e0c79b2e8a4a9b6cdbf528dcc323e
thiagomarchesi <181411010+thiagomarchesi@users.noreply.github.com>
2024-09-25 13:12:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,14 +10,12 @@ st.write(car_data)
 
 hist_button = st.button('Criar histograma')
 
-bins = [0, 10000, 20000, 30000, 40000, 50000, 100000]
-
 if hist_button:
     
     st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
       
-    fig = px.histogram(car_data, y="price", bins=bins, opacity=0.5)
-    
+    fig = px.histogram(car_data, x="type", y="price", ybins=[0, 10000, 20000, 30000, 40000, 50000, 100000], opacity=0.5)
+      
     st.plotly_chart(fig, use_container_width=True)
 
 scatter_button = st.button('Criar gráfico de dispersão')
