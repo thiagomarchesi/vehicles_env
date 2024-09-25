1f27bf6e4da1143672111f740ee800061f72dff3
thiagomarchesi <181411010+thiagomarchesi@users.noreply.github.com>
2024-09-25 12:35:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if hist_button:
     
     st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
       
-    fig = px.histogram(car_data, x="price", nbins=20)
+    fig = px.histogram(car_data, x="type", nbins=20, opacity=0.3)
     
     st.plotly_chart(fig, use_container_width=True)
 
@@ -22,5 +22,5 @@ scatter_button = st.button('Criar gráfico de dispersão')
 
 if scatter_button:
     st.write('Criando um gráfico de dispersão para o conjunto de dados')
-    fig = px.scatter(car_data, x="price", y="type")
+    fig = px.scatter(car_data, x="type", y="price")
     st.plotly_chart(fig, use_container_width=True)
